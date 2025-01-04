8603a1ff435ca1377d2c6f64f36c84ef7090bcb2
M0hc3n <mouhcen.chouireb@ensia.edu.dz>
2025-01-04 21:53:06 +0100

    pushing

--- src/app.py
+++ src/app.py
@@ -36,4 +36,4 @@ def build_game():
     return make_response(jsonify({"data": response_data}), status["success"])
 
 # if __name__ == "__main__":
-app.run(debug=True, port=10000, host="0.0.0.0")
+app.run(debug=False, port=10000, host="0.0.0.0")
